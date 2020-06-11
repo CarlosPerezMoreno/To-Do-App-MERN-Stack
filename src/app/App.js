8cdbb3e5154938dceb10ca45821bd27e362c0bbc
Carlos <carlos.perez.moreno.02@gmail.com>
2020-06-11 20:09:33 +0200

    feat: Packages changed. App component initialized
    Dependencies had to be changed. Babel and webpack configured. Materialize css framework added. Component App started

---
+++ src/app/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="light-blue darken-4">
+          <div className="container">
+            <a className="brand-logo" href="/">
+              {" "}
+              Taking Notes!
+            </a>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use("/api/tasks", require("./routes/task.routes"));
 
 //Static
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 //Starting server
 
